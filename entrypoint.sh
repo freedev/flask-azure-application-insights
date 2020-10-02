b8aa7cdb6f9638e29f37cd64fd61c1c8458db359
Vincenzo D'Amore <v.damore@gmail.com>
2020-10-02 12:59:24 +0200

    first import

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,5 +19,5 @@ echo "threads n. " $GU_THREADS_NUM
 echo "processes n. " $GU_WORK_NUM
 
 # threading influenced by https://medium.com/building-the-system/gunicorn-3-means-of-concurrency-efbb547674b7
-# GUNICORN_CMD_ARGS="--bind=127.0.0.1 --workers=$GU_WORK_NUM --threads=$GU_THREADS_NUM --preload" gunicorn -b 0.0.0.0:5000 application:app
-python application.py
+GUNICORN_CMD_ARGS="--bind=127.0.0.1 --workers=$GU_WORK_NUM --threads=$GU_THREADS_NUM --preload" gunicorn -b 0.0.0.0:5000 application:app
+# python application.py
